64b52c07140f6c7d558912745397b2ee4a36265e
Migeru65 <justin_valenzuela@dlsu.edu.ph>
2026-03-16 15:03:06 +0800

    commit

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,25 +1,31 @@
-const { merge } = require('webpack-merge');
-const common = require('./webpack.common.js');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CopyPlugin = require('copy-webpack-plugin');
+const { merge } = require("webpack-merge");
+const common = require("./webpack.common.js");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = merge(common, {
-  mode: 'production',
+  mode: "production",
   plugins: [
     new HtmlWebpackPlugin({
-      template: './index.html',
+      template: "./index.html",
     }),
     new CopyPlugin({
       patterns: [
-        { from: 'img', to: 'img' },
-        { from: 'css', to: 'css' },
-        { from: 'js/vendor', to: 'js/vendor' },
-        { from: 'icon.svg', to: 'icon.svg' },
-        { from: 'favicon.ico', to: 'favicon.ico' },
-        { from: 'robots.txt', to: 'robots.txt' },
-        { from: 'icon.png', to: 'icon.png' },
-        { from: '404.html', to: '404.html' },
-        { from: 'site.webmanifest', to: 'site.webmanifest' },
+        { from: "img", to: "img" },
+        { from: "css", to: "css" },
+        { from: "js/jquery.js", to: "js/jquery.js" },
+        { from: "js/bootstrap.min.js", to: "js/bootstrap.min.js" },
+        { from: "js/jquery.parallax.js", to: "js/jquery.parallax.js" },
+        { from: "js/smoothscroll.js", to: "js/smoothscroll.js" },
+        { from: "js/wow.min.js", to: "js/wow.min.js" },
+        { from: "js/custom.js", to: "js/custom.js" },
+        { from: "clock.html", to: "clock.html" },
+        { from: "icon.svg", to: "icon.svg" },
+        { from: "favicon.ico", to: "favicon.ico" },
+        { from: "robots.txt", to: "robots.txt" },
+        { from: "icon.png", to: "icon.png" },
+        { from: "404.html", to: "404.html" },
+        { from: "site.webmanifest", to: "site.webmanifest" },
       ],
     }),
   ],
